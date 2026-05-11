830a4c2115991d587e6639a2a36ea8eebf2f647f
Amey Kawale <ameykawale345@gmail.com>
2026-05-12 00:00:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import smtplib
+import random
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 CORS(app)
+
+# EMAIL CONFIG
+EMAIL_SENDER = "smart.codemark.attendance@gmail.com"
+EMAIL_PASSWORD = "bfsq fiaj felf pcwy"
 
 # DATABASE
 def get_db():
@@ -11,7 +18,7 @@ def get_db():
     conn.row_factory = sqlite3.Row
     return conn
 
-# CREATE TABLES
+# CREATE TABLE
 conn = get_db()
 
 conn.execute("""
@@ -34,11 +41,14 @@ def home():
 # REGISTER ROUTE
 @app.route("/register", methods=["POST"])
 def register():
+
     data = request.get_json()
 
     name = data.get("name")
     email = data.get("email")
     password = data.get("password")
+
+    otp = str(random.randint(100000, 999999))
 
     conn = get_db()
 
@@ -50,8 +60,33 @@ def register():
     conn.commit()
     conn.close()
 
+    try:
+        msg = MIMEText(f"Your SmartAttend OTP is: {otp}")
+
+        msg["Subject"] = "SmartAttend OTP"
+        msg["From"] = EMAIL_SENDER
+        msg["To"] = email
+
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+
+        server.login(EMAIL_SENDER, EMAIL_PASSWORD)
+
+        server.sendmail(
+            EMAIL_SENDER,
+            email,
+            msg.as_string()
+        )
+
+        server.quit()
+
+    except Exception as e:
+        return jsonify({
+            "message": str(e)
+        }), 500
+
     return jsonify({
-        "message": "Registration successful"
+        "message": "OTP sent successfully",
+        "otp": otp
     })
 
 # RUN
